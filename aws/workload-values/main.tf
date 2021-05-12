37c54dddeab799bce96bef817ad0b8f80ec786f4
Joe Ferris <jferris@thoughtbot.com>
2021-05-12 19:27:50 -0400

    Use domain filter for allowed Route 53 zones

--- aws/workload-values/main.tf
+++ aws/workload-values/main.tf
@@ -1,10 +1,11 @@
 module "dns_service_account_role" {
   source = "../dns-service-account-role"
 
-  aws_namespace = var.aws_namespace
-  aws_tags      = var.aws_tags
-  k8s_namespace = var.k8s_namespace
-  oidc_issuer   = var.oidc_issuer
+  aws_namespace    = var.aws_namespace
+  aws_tags         = var.aws_tags
+  k8s_namespace    = var.k8s_namespace
+  oidc_issuer      = var.oidc_issuer
+  route53_zone_ids = values(data.aws_route53_zone.managed).*.id
 }
 
 module "cluster_autoscaler_service_account_role" {
@@ -14,6 +15,12 @@ module "cluster_autoscaler_service_account_role" {
   aws_tags      = var.aws_tags
   k8s_namespace = var.k8s_namespace
   oidc_issuer   = var.oidc_issuer
+}
+
+data "aws_route53_zone" "managed" {
+  for_each = toset(var.domain_filters)
+
+  name = each.value
 }
 
 data "aws_region" "current" {}
