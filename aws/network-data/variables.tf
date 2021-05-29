3756d67a83670a6c9cc95a9f26f7efa2b46b02ae
Joe Ferris <jferris@thoughtbot.com>
2021-05-29 15:06:30 -0400

    Set defaults for public/private tags

--- aws/network-data/variables.tf
+++ aws/network-data/variables.tf
@@ -1,11 +1,13 @@
 variable "private_tags" {
   description = "Tags to identify private subnets"
   type        = map(string)
+  default     = { Topology = "private" }
 }
 
 variable "public_tags" {
   description = "Tags to identify public subnets"
   type        = map(string)
+  default     = { Topology = "public" }
 }
 
 variable "network_tags" {
