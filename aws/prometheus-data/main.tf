152951378d9c70641b0cf25f90846772a751648f
Olamide <65307752+OlamideOl1@users.noreply.github.com>
2022-04-29 16:58:59 +0100

    Create a prometheus_data module to build a datasource object to connect to AWS prometheus workspace (#93)
    * Create a prometheus_data module to build a datasource object for AWS prometheus workspace

---
+++ aws/prometheus-data/main.tf
@@ -0,0 +1,17 @@
+data "aws_caller_identity" "this" {}
+
+data "aws_s3_bucket_object" "prometheus" {
+
+  bucket = join("-", concat([
+    "prometheus",
+    var.aws_prometheus_workspace_name,
+    data.aws_caller_identity.this.account_id
+  ]))
+
+  key = "ingestion.json"
+}
+
+locals {
+  prometheus_workspace_json = join("", data.aws_s3_bucket_object.prometheus.*.body)
+  prometheus_data           = jsondecode(local.prometheus_workspace_json)
+}
