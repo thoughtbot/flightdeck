21b2f4fddf64c41b85ca8c1c0f0222846fa6bbe3
Olamide <olamide@thoughtbot.com>
2022-12-22 16:50:09 +0100

    Create service account role for EBS CSI driver

---
+++ aws/platform/modules/ebs-csi-driver-service-account-role/README.md
@@ -0,0 +1,4 @@
+# Prometheus Service Account Role
+
+Configures an IAM role for a EBS csi driver service account. This role is designed
+to be used with a EBS csi driver deployed in the kubernetes cluster.
---
+++ aws/platform/modules/ebs-csi-driver-service-account-role/main.tf
@@ -0,0 +1,42 @@
+module "ebs_csi_driver_service_account_role" {
+  source = "../../../service-account-role"
+
+  name             = "ebs-csi-controller-sa"
+  namespace        = var.aws_namespace
+  oidc_issuers     = [var.oidc_issuer]
+  service_accounts = ["${var.k8s_namespace}:ebs-csi-controller-sa"]
+  tags             = var.aws_tags
+}
+
+resource "aws_iam_policy" "this" {
+  name   = module.ebs_csi_driver_service_account_role.name
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = module.ebs_csi_driver_service_account_role.name
+  policy_arn = aws_iam_policy.this.arn
+}
+
+resource "aws_iam_role_policy_attachment" "managed_policy" {
+  role       = module.ebs_csi_driver_service_account_role.name
+  policy_arn = "${local.policy_prefix}/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+data "aws_iam_policy_document" "ebs_csi_driver_encryption" {
+  statement {
+    sid = "EncryptEbsStoragePolicy"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:CreateGrant"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_partition" "current" {}
+
+locals {
+  policy_prefix = "arn:${data.aws_partition.current.partition}:iam::aws:policy"
+}
