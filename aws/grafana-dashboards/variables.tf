fca7cfeaa255edb89c95d491bc2b6e4aed3634e5
Clarissa Borges <cborges@thoughtbot.com>
2023-06-23 17:43:41 -0300

    Create grafana-dashboards module
    This module will allow that we create some basic dashboards. We're
    adding the option to create the istio dashboard for now.

---
+++ aws/grafana-dashboards/variables.tf
@@ -0,0 +1,5 @@
+variable "create_istio_dashboard" {
+  description = "Create Istio dashboard"
+  type        = bool
+  default     = false
+}
---
+++ aws/grafana-dashboards/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.14.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 1.13"
+    }
+  }
+}
