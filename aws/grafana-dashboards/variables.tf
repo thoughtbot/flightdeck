5b0bac966c4b25cf41e88eafdbb17ee72adc7bc6
Clarissa Lima Borges <cborges@thoughtbot.com>
2023-06-27 11:02:42 -0300

    Allow more dashboards to be created (SLO, Cluster Resources and RDS databases)
    Creating more generic dashboards with handy panels for monitoring the
    overall application performance.

--- aws/grafana-dashboards/variables.tf
+++ aws/grafana-dashboards/variables.tf
@@ -1,5 +1,17 @@
-variable "create_istio_dashboard" {
-  description = "Create Istio dashboard"
-  type        = bool
-  default     = false
+variable "dashboards_to_create" {
+  description = "List of dashboards to create (cluster-resources, istio, rds-databases, slo-details, and/or slo-overview)"
+  type        = list(string)
+
+  validation {
+    condition = length(setsubtract(
+      var.dashboards_to_create,
+      ["cluster-resources", "istio", "rds-databases", "slo-details", "slo-overview"],
+    )) == 0
+    error_message = "Valid options for dashboards to create are: cluster-resources, istio, rds-databases, slo-details, slo-overview"
+  }
+
+  validation {
+    condition     = length(var.dashboards_to_create) > 0
+    error_message = "At least one dashboard must be selected"
+  }
 }
