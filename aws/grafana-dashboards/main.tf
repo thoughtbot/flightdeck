347d71e060dd68404394b2c28a6d533f86c6384e
Clarissa Lima Borges <cborges@thoughtbot.com>
2023-06-28 11:45:36 -0300

    Prevent overriding changes to dashboards created using the module (#150)
    Because of how Grafana dashboards are managed, it's doubtful that someone will make changes to the JSON version of the dashboards rather than make updates directly to the UI. The goal of the grafana-dashboards module is to create the starter dashboards, and then the user can customize them as they would like without sticking to Flightdeck's initial versions.
    
    This change will ensure that once the dashboards are created, terraform won't try to revert changes that the user makes to the UI whenever they try to make changes to resources that use this module.

--- aws/grafana-dashboards/main.tf
+++ aws/grafana-dashboards/main.tf
@@ -2,4 +2,10 @@ resource "grafana_dashboard" "this" {
   for_each = toset(var.dashboards_to_create)
 
   config_json = file("${path.module}/dashboards/${each.value}.json")
+
+  lifecycle {
+    ignore_changes = [
+      config_json
+    ]
+  }
 }
