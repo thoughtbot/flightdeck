66db8f130c511d18273568fb1393802fb516b3e8
Olamide <olamide@thoughtbot.com>
2023-10-30 10:39:16 +0100

    Use chart repository for cloudwatch adapter

--- platform/modules/cloudwatch-adapter/main.tf
+++ platform/modules/cloudwatch-adapter/main.tf
@@ -1,7 +1,12 @@
 resource "helm_release" "cloudwatch_adapter" {
-  chart     = "${path.module}/chart"
-  name      = var.name
-  namespace = var.k8s_namespace
-  values    = var.chart_values
+  chart      = coalesce(var.chart_name, local.chart_defaults.chart)
+  name       = var.name
+  namespace  = var.k8s_namespace
+  repository = coalesce(var.chart_repository, local.chart_defaults.repository)
+  values     = var.chart_values
+  version    = coalesce(var.chart_version, local.chart_defaults.version)
 }
 
+locals {
+  chart_defaults = jsondecode(file("${path.module}/chart.json"))
+}
