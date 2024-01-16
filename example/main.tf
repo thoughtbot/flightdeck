be0c376dcfa22373dafe1da790ececa5800d121c
Christi Miller <christi_miller@outlook.com>
2024-01-16 15:24:25 -0700

    Adding ECR

--- example/main.tf
+++ example/main.tf
@@ -12,27 +12,16 @@ terraform {
 
 # Add the AWS provider
 provider "aws" {
-  region = "us-west-2" 
+  region = "ADDME" 
+	profile = "ADDME"
 }
 
-# Flightdeck Module for EKS Cluster.  You'll reference the Flightdeck repository and specify the required parameters.
-module "eks_cluster" {
-  source = "github.com/thoughtbot/flightdeck//aws/eks?ref=latest"
 
+resource "aws_ecr_repository" "flightdeck_example_ecr_repo" {
+  name                 = "flightdeck-example"  # Replace with your repository name
+  image_tag_mutability = "MUTABLE"
 
-	# Replace "flightdeck-example-eks-cluster" with your desired cluster name and fill in other configurations as needed.
-  cluster_name = "flightdeck-example-eks-cluster"
-
-  # Define other necessary configurations for your cluster
-}
-
-# Flightdeck Platform Module.  Add the Flightdeck platform module to configure the Kubernetes setup on the EKS cluster.
-module "flightdeck_platform" {
-  source = "github.com/thoughtbot/flightdeck//aws/platform?ref=latest"
-
-  cluster_name = module.eks_cluster.cluster_name
-	admin_roles  = ["arn:aws:iam::123456789012:role/AdminRole"]
-
-  # Specify other required configurations
-  # Define additional configurations for the platform
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
