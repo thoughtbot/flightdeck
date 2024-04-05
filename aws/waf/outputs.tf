d9c0122085a360d2ee768a6ad751b040b7ebcb80
Olamide <Olamide@thoughtbot.com>
2024-04-05 13:28:13 +0100

    Remove unused output for sns topic

--- aws/waf/outputs.tf
+++ aws/waf/outputs.tf
@@ -3,7 +3,3 @@ output "aws_waf_arn" {
   value       = aws_wafv2_web_acl.main.arn
 }
 
-output "waf_logs_sns_topic_arn" {
-  description = "The arn for the SNS topic to receive the AWS WAF logs"
-  value       = aws_sns_topic.waf_logs_sns_subscription.arn
-}
