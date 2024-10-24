d0b1811b7cb2ff936ccaa9ce05d9d9d495a526fb
Olamide <65307752+OlamideOl1@users.noreply.github.com>
2024-10-24 17:19:43 +0100

    Grant cluster wide crd access to deploy roles (#214)
    * Grant cluster wide crd access to deploy roles
    
    * terraform-docs: automated action
    
    * update terraform fmt
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- aws/deploy-role-bindings/main.tf
+++ aws/deploy-role-bindings/main.tf
@@ -1,9 +1,8 @@
-resource "kubernetes_role_binding" "cluster" {
+resource "kubernetes_cluster_role_binding" "cluster" {
   for_each = toset(var.cluster_roles)
 
   metadata {
-    name      = var.name
-    namespace = var.namespace
+    name = var.name
   }
 
   role_ref {
@@ -17,6 +16,38 @@ resource "kubernetes_role_binding" "cluster" {
     name      = var.group
     api_group = "rbac.authorization.k8s.io"
   }
+}
+
+resource "kubernetes_cluster_role" "cluster_crd" {
+  metadata {
+    name = "${var.name}-cluster-crd"
+  }
+
+  rule {
+    api_groups = ["apiextensions.k8s.io"]
+    resources  = ["customresourcedefinitions"]
+    verbs      = ["get", "list"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "cluster_crd" {
+  metadata {
+    name = "${var.name}-cluster-crd"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "${var.name}-cluster-crd"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = var.group
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  depends_on = [kubernetes_cluster_role.cluster_crd]
 }
 
 resource "kubernetes_role_binding" "crd" {
