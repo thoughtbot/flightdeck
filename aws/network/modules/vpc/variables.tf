b63322ce9c09f56e436d69d2c912640f579e691d
Benjamin Valiente <79182724+benjaminvaliente@users.noreply.github.com>
2026-03-24 08:40:15 -0600

    feat: adding retention support to Cloudwatch log group (#266)
    * feat: adding retention support to Cloudwatch log group
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: Benjamin Valiente (Contractor) <benjamin.valiente@thoughtbot.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- aws/network/modules/vpc/variables.tf
+++ aws/network/modules/vpc/variables.tf
@@ -31,3 +31,9 @@ variable "cidr_block" {
   type        = string
   description = "CIDR block to be used for the VPC, such as 10.0.0.0/16"
 }
+
+variable "flow_logs_retention_days" {
+  description = "Number of days to retain VPC Flow Logs in CloudWatch." # Only applies when enable_flow_logs is true.
+  type        = number
+  default     = null # null = retain forever
+}
