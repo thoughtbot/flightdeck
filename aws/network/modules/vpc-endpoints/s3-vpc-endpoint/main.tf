a9d7de06c1b317151ad744ee1fc09f238f35d83a
Benjamin Valiente <79182724+benjaminvaliente@users.noreply.github.com>
2026-04-13 18:34:57 -0600

    fix: updating deprecated region attribute (#269)
    Co-authored-by: Benjamin Valiente (Contractor) <benjamin.valiente@thoughtbot.com>

--- aws/network/modules/vpc-endpoints/s3-vpc-endpoint/main.tf
+++ aws/network/modules/vpc-endpoints/s3-vpc-endpoint/main.tf
@@ -2,7 +2,7 @@ data "aws_region" "current" {}
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = var.route_table_ids
